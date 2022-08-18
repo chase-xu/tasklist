ce0c7f5082ea65b1186dbc7699ca41ea60e4db08
Chase <pengxu1993@gmail.com>
2022-08-17 18:00:37 -0700

    set port to env

--- server/App.js
+++ server/App.js
@@ -26,8 +26,8 @@ app.use(notFound);
 app.use(errorHandler);
 // app.use(rateLimit);
 
-// const port = process.env.PORT;
-const port = 8090;
+const port = process.env.PORT;
+// const port = 8090;
 const start = async ()=>{
     try{
         await connectDB(process.env.MONGO_URI)
